107a38b1a3c84b72686f18b43980a4bcbd52f84c
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-10-06 16:57:58 -0300

    II. SOBRE VETORES E MATRIZES
    2. Ler um vetor V de n posições (aceitar somente números positivos) via teclado. Escrever a seguir o valor do maior elemento de V e a respectiva posição que ele ocupa no vetor.

---
+++ 1.2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+int *V, n, maior, posicao;
+
+int *GeraVetor(int tam, int *vetor){
+	vetor=(int*)malloc(tam*sizeof(int));
+	return vetor;
+}
+
+int *EntraVetor(int tam, int *vetor){
+    for(int j=0; j<tam; j++){
+    maior = 0;
+    do {
+    cout<<"Digite o valor da posição "<<j+1<<": ";
+    cin>>vetor[j];}
+    while (vetor[j] < 0);
+	}
+          
+    return vetor;
+}
+	
+void MostraMaiorePos(int tam, int *vetor){
+	maior = vetor[0];	
+	for(int j=0; j<tam; j++){
+		if(vetor[j]> maior) maior = vetor[j];
+		posicao = vetor[j];
+	}
+	cout<<"O maior elemento é: "<<maior <<"\nNa posição: "<< posicao <<endl;
+    }
+
+void MostraVetor(int tam, int *vetor){
+    cout<<"IMPRIMINDO VETOR COM "<<tam<<" POSICOES"<<endl<<endl;
+    for(int j=0; j<tam; j++){
+    if (j==0) cout<<"v=["<<vetor[j]<<","; else
+        if(j<n-1) cout << vetor[j] << ","; else
+            cout<<vetor[j]<<"]"<< endl;
+    }
+}
+
+int main(){
+    cout <<"Qual o tamanho do vetor? ";
+    cin >> n;
+    V =GeraVetor(n,V);
+    V =EntraVetor(n,V);
+	MostraVetor(n,V);
+    MostraMaiorePos(n,V);
+    
+	free(V);
+	return 0;
+}
