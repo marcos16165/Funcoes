5bac822d63d60687889b1acdc27d7de2db39094f
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-10-06 14:57:27 -0300

    II. SOBRE VETORES E MATRIZES
    1. Elabore um programa que encontre o menor e o maior elemento de um vetor com n elementos, sendo 𝑛 informado na entrada do programa.

---
+++ 1.1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+int *V, n, maior, menor;
+
+int *GeraVetor(int tam, int *vetor){
+    vetor=(int*)malloc(tam*sizeof(int));
+    return vetor;
+}
+    
+int *EntraVetor(int tam, int *vetor){
+    for(int j=0; j<tam; j++){
+        cout<<"Digite o valor da posição: "<<j+1<<": ";
+        cin>>vetor[j];
+        }
+        return vetor;
+    }
+    
+void MostraMenor(int tam, int *vetor){
+	menor = vetor[0];
+	for(int j=0; j<tam; j++){
+		if(vetor[j]<menor) menor = vetor[j];
+	}
+	cout<<"O menor elemento é: "<<menor << endl;
+    }
+    
+void MostraMaior(int tam, int *vetor){
+	maior = vetor[0];	
+	for(int j=0; j<tam; j++){
+		if(vetor[j]> maior) maior = vetor[j];
+	}
+	cout<<"O maior elemento é: "<<maior << endl;
+    }
+
+void MostraVetor(int tam, int *vetor){
+    cout<<"IMPRIMINDO VETOR COM "<<tam<<" POSICOES"<<endl<<endl;
+    for(int j=0; j<tam; j++){
+    if (j==0) cout<<"v=["<<vetor[j]<<","; else
+        if(j<n-1) cout << vetor[j] << ","; else
+            cout<<vetor[j]<<"]"<< endl;
+    }
+    
+    cout<<"------------------------------------------------" <<endl;    
+}
+int main()
+{
+    cout <<"Qual o tamanho do vetor? ";
+    cin >> n;
+    V =GeraVetor(n,V);
+    V =EntraVetor(n,V);
+    MostraVetor(n,V);
+    MostraMaior(n,V);
+    MostraMenor(n,V);
+    
+    free(V);
+    return 0;
+}
