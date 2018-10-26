65e56b1c171e8254a638f1947cde900e1f6f81f2
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-10-26 18:44:36 -0300

    II. SOBRE VETORES E MATRIZES

--- 11.cpp
+++ 11.cpp
@@ -7,63 +7,53 @@ using namespace std;
 char *gabarito, **matriz;
 int *acertos, linha, col, a;
 
-int *GeraVetor(int tam, int *vetor)
-{
-	vetor = (int*)malloc(tam * sizeof(int));
-	return vetor;
+int *GeraVetor(int tam, int *vetor){
+    vetor=(int*)malloc(tam*sizeof(int));
+    return vetor;
 }
 
 
-char *GeraGabarito(int tam, char *vetor)
-{
-	vetor = (char*)malloc(tam * sizeof(char));
-	return vetor;
+char *GeraGabarito(int tam, char *vetor){
+    vetor=(char*)malloc(tam*sizeof(char));
+    return vetor;
 }
 
 char **GeraMatriz(int linha, int col, char **matriz)
 {
 	matriz = (char**)malloc(col * sizeof(char));
-
+	
 	for(int j = 0; j < linha; j++)
 		matriz[j] = (char*)malloc(col * sizeof(char));
 	return matriz;
 }
 
-char *EntraVetor(int tam, char *vetor)
-{
-	for(int i = 0; i < 10; i++)
-	{
-		cout << "Questao " << i + 1 << ":\nMARQUE a, b, c ou d = ";
-		cin >> gabarito[i];
+char *EntraVetor(int tam, char *vetor){
+ 	for(int i=0; i<tam; i++){
+		cout<<"Questao "<< i + 1<<":\nMarque a, b, c ou d = ";
+		cin>>gabarito[i];
 	}
-
-	return vetor;
-}
-
-char **EntraMatriz (int linha, int coluna, char **matriz, int *vetor)
-{
-	for(int i = 0; i < 100; i++)
-	{
-		vetor[i] = 0;
-		cout << "INSIRA AS RESPOSTAS DOS ALUNOS " << i + 1 << "." << endl;
-		for(int j = 0; j < 10; j++)
-		{
-			cout << "Questao " << j + 1 << ".";
-			cin >> matriz[i][j];
-			if(matriz[i][j] == gabarito[i])
-			{
-				vetor[i] = vetor[i] + 1;
+	
+        return vetor;
+    }
+	
+char **EntraMatriz (int linha, int coluna, char **matriz, int *vetor){	
+	for(int i=0; i<linha; i++){
+		vetor[i]=0;
+		cout<<"Insira as respostas do aluno "<<i+1<<"."<<endl;
+		for(int j=0;j<col;j++){
+			cout<<"Questao "<<j+1<<".";
+			cin>>matriz[i][j];
+			if(matriz[i][j] == gabarito[i]){
+				vetor[i] = vetor[i]+1;
 			}
-		}
+		}		
 	}
 
 	return matriz;
 }
 
-void MostraMatriz(int linha, int col, int **matriz)
-{
-	for(int i = 0; i < linha; i++)
-	{
+void MostraMatriz(int linha, int col, int **matriz){
+	for(int i = 0; i < linha; i++){
 		cout << endl;
 		if(i == 0) cout << "[" ;
 		for(int j = 0; j < col; j++)
@@ -77,44 +67,44 @@ void MostraMatriz(int linha, int col, int **matriz)
 	cout << "]" << endl;
 }
 
-void MostraVetor(int tam, int *vetor)
-{
-	cout << "NOTAS DOS ALUNOS ORDENADAS: " << endl;
-	for(int j = 0; j < tam; j++)
-	{
-		if (j == 0) cout << "V=[" << vetor[j] << ",";
-		else if (j < tam - 1) cout << vetor[j] << ", ";
-		else
-			cout << vetor[j] << "]" << endl;
-	}
-}
+void MostraVetor(int tam, int *vetor){
+	cout<<"Notas dos alunos em ordem: "<<endl;
+    for(int j=0; j<tam; j++){
+    if (j==0) cout<<"V=["<<vetor[j]<<","; else
+        if (j<tam-1) cout<<vetor[j]<<", "; else
+          cout<<vetor[j]<<"]"<<endl;
+          }
+      }
 
-void LiberaMatriz(int linha, char **matriz)
+ void LiberaMatriz(int linha, char **matriz)
 {
 	for (int i = 0; i < linha; i++)
 		free(matriz[i]);
 	free(matriz);
 }
-
-
+  
+    
 
 using namespace std;
 
-int main()
-{
-	linha = 100;
-	col = 10;
-	a = 10;
+int main(){
+linha = 100;
+col = 10;
+a = 10;
 
-	acertos = GeraVetor(100, acertos);
-	gabarito = GeraGabarito(a, gabarito);
-	cout << "CRIAR GABARITO" << endl;
-	gabarito = EntraVetor(10, gabarito);
-	matriz = GeraMatriz(linha, col, matriz);
-	matriz = EntraMatriz(linha, col, matriz, acertos);
-	MostraVetor(a, acertos);
-	LiberaMatriz(linha, matriz);
-	free(acertos);
+acertos = GeraVetor(100,acertos);
+gabarito = GeraGabarito(a,gabarito);
+cout<<"Crie o gabarito"<<endl;
+gabarito = EntraVetor(10,gabarito);
+matriz = GeraMatriz(linha, col,matriz);
+matriz = EntraMatriz(linha,col,matriz,acertos);
+MostraVetor(a,acertos);
+LiberaMatriz(linha, matriz);
+free(acertos);
+
+	
+	return 0;
+}
 
 
 	return 0;
