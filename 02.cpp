51cbf66f16f8b9e759e74b8db4d8e0a47372bc25
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-10-26 13:33:08 -0300

    I.  SOBRE VETORES E MATRIZES

--- 02.cpp
+++ 02.cpp
@@ -1,55 +1,6 @@
-#include <iostream>
-#include <math.h>
-#include <stdlib.h>
+//Uma maneira bem simples de preencher uma matriz M mxn, que no caso é 3x4, é usando o laço "for".
 
-using namespace std;
-int *V, n, maior, posicao;
-
-int *GeraVetor(int tam, int *vetor){
-	vetor=(int*)malloc(tam*sizeof(int));
-	return vetor;
-}
-
-int *EntraVetor(int tam, int *vetor){
-    for(int j=0; j<tam; j++){
-    maior = 0;
-    do {
-    cout<<"Digite o valor da posiÃ§Ã£o "<<j+1<<": ";
-    cin>>vetor[j];}
-    while (vetor[j] < 0);
-	}          
-    return vetor;
-}
-
-void MostraMaiorePos(int tam, int *vetor){
-	maior = vetor[0];
-	posicao = 0;	
-	for(int j=0; j<tam; j++){
-		if(vetor[j]> maior){ 
-		maior = vetor[j];
-		posicao = j;
-		}
-	}
-	cout<<"O maior elemento e: "<<maior <<"\nNa posicao: "<< posicao + 1 <<endl;
-    }
-
-void MostraVetor(int tam, int *vetor){
-    cout<<"IMPRIMINDO VETOR COM "<<tam<<" POSICOES"<<endl<<endl;
-    for(int j=0; j<tam; j++){
-    if (j==0) cout<<"v=["<<vetor[j]<<","; else
-        if(j<n-1) cout << vetor[j] << ","; else
-            cout<<vetor[j]<<"]"<< endl;
-    }
-}
-
-int main(){
-    cout <<"Qual o tamanho do vetor? ";
-    cin >> n;
-    V =GeraVetor(n,V);
-    V =EntraVetor(n,V);
-	MostraVetor(n,V);
-    MostraMaiorePos(n,V);
-    
-	free(V);
-	return 0;
-}
+For (int i=0; i<3; i++)
+	For (int =0; j<4; ++j)
+{
+	cin>>matriz[ i ][ j ]; //Libera 12 espaços para serem preenchidos pelo usuário. 
