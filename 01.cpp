144b07379fb2bb46dd4735f4f86966f606a8fe81
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-10-26 13:23:49 -0300

    I.  SOBRE VETORES E MATRIZES

--- 01.cpp
+++ 01.cpp
@@ -1,58 +1,12 @@
-#include <iostream>
-#include <math.h>
-#include <stdlib.h>
+//Usando vetores: 
+//Uma pessoa deseja armazenar, a idade de todos os seus familiares 
+//e em seguida saber quem é o mais velho e o mais novo.
 
-using namespace std;
-int *V, n, maior, menor;
+	int a[n]; // "n" será a quantidade de elementos do vetor.
+	int maior, menor;
 
-int *GeraVetor(int tam, int *vetor){
-    vetor=(int*)malloc(tam*sizeof(int));
-    return vetor;
-}
-    
-int *EntraVetor(int tam, int *vetor){
-    for(int j=0; j<tam; j++){
-        cout<<"Digite o valor da posiÃ§Ã£o: "<<j+1<<": ";
-        cin>>vetor[j];
-        }
-        return vetor;
-    }
-    
-void MostraMenor(int tam, int *vetor){
-	menor = vetor[0];
-	for(int j=0; j<tam; j++){
-		if(vetor[j]<menor) menor = vetor[j];
-	}
-	cout<<"O menor elemento Ã©: "<<menor << endl;
-    }
-    
-void MostraMaior(int tam, int *vetor){
-	maior = vetor[0];	
-	for(int j=0; j<tam; j++){
-		if(vetor[j]> maior) maior = vetor[j];
-	}
-	cout<<"O maior elemento Ã©: "<<maior << endl;
-    }
+//Usando matrizes: 
+//Uma professora precisa armazenar a nota de 4 alunos que ficaram em recuperação, 
+//3 notas das unidades e a nota da recuperação, assim ela vai precisar de uma matriz 4x4.
 
-void MostraVetor(int tam, int *vetor){
-    cout<<"IMPRIMINDO VETOR COM "<<tam<<" POSICOES"<<endl<<endl;
-    for(int j=0; j<tam; j++){
-    if (j==0) cout<<"v=["<<vetor[j]<<","; else
-        if(j<n-1) cout << vetor[j] << ","; else
-            cout<<vetor[j]<<"]"<< endl;
-    }
-    
-    cout<<"------------------------------------------------" <<endl;    
-}
-int main()
-{
-    cout <<"Qual o tamanho do vetor? ";
-    cin >> n;
-    V =GeraVetor(n,V);
-    V =EntraVetor(n,V);
-    MostraVetor(n,V);
-    MostraMaior(n,V);
-    MostraMenor(n,V);    
-    free(V);
-    return 0;
-}
+   	float M[4][4];
