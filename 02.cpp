fc6695e0917277eae454e922b73c8dfc2eb7fa58
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-11-13 09:28:21 -0300

    Rename 02.cpp to 2.cpp

--- 02.cpp
+++
@@ -1,6 +0,0 @@
-//Uma maneira bem simples de preencher uma matriz M mxn, que no caso é 3x4, é usando o laço "for".
-
-For (int i=0; i<3; i++)
-	For (int =0; j<4; ++j)
-{
-	cin>>matriz[ i ][ j ]; //Libera 12 espaços para serem preenchidos pelo usuário. 
