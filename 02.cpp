58cab6f5d667a25a6848fbcbcaedf2d365bb8a67
marcos16165 <38138237+marcos16165@users.noreply.github.com>
2018-11-13 10:14:54 -0300

    Rename 1.2.cpp to 02.cpp

--- 02.cpp
+++ 02.cpp
@@ -13,7 +13,7 @@ int *GeraVetor(int tam, int *vetor){
 int *EntraVetor(int tam, int *vetor){
     for(int j=0; j<tam; j++){
     do {
-    cout<<"Digite o valor da posição "<<j+1<<": ";
+    cout<<"Digite o valor da posiÃ§Ã£o "<<j+1<<": ";
     cin>>vetor[j];}
     while (vetor[j] < 0);
 	}
